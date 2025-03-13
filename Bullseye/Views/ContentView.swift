4999d1422713eb256a5100b549c713edb7bab436
Mina Rezkalla <minashakeralfy@gmail.com>
2025-03-13 22:05:56 +0100

    Organize the code and learning swift ui and data

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -6,6 +6,8 @@ struct ContentView: View {
   @State private var alertIsVisible: Bool = false
   @State private var sliderValue : Double = 10.0
   
+  @State private var game :Game = Game()
+  
   var body: some View {
     VStack{
       Text("🎯🎯🎯 \n PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
@@ -15,7 +17,7 @@ struct ContentView: View {
         .font(.footnote)
         .kerning(2.0)
         
-      Text("89")
+      Text(String(game.target))
         .kerning(-1.0)
         .font(.largeTitle)
         .fontWeight(.black)
@@ -37,7 +39,11 @@ struct ContentView: View {
         }
       },
         message: {
-        Text("This is my first alert!")
+        let rounderValue :Int = Int(sliderValue.rounded())
+        Text("""
+              The slider's value is \(rounderValue)
+             You scored \(game.points(sliderValue: rounderValue)) points this round
+             """)
       })
     }
     
