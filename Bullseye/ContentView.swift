051103d5dc40bb3824f56b79f09daa8f319eab78
Mina Rezkalla <minashakeralfy@gmail.com>
2025-03-13 19:30:37 +0100

    initial commit

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -3,15 +3,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @State private var alertIsVisible: Bool = false
+  var body: some View {
+    VStack{
+      Text("🎯🎯🎯 \n PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        .bold()
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
+        .kerning(2.0)
+        
+      Text("89")
+        .kerning(-1.0)
+        .font(.largeTitle)
+        .fontWeight(.black)
+        .bold()
+      HStack{
+        Text("1")
+          .bold()
+        Slider(value:.constant(50) ,in: 1.0...100.0)
+        Text("100")
+          .bold()
+      }
+      Button("Hit me") {
+        alertIsVisible = true
+      }.alert("Hello there!",
+              isPresented: $alertIsVisible,
+              actions: {
+        Button("Awesome") {
+          print("Alert closed")
         }
-        .padding()
+      },
+        message: {
+        Text("This is my first alert!")
+      })
     }
+    
+  }
 }
 
 #Preview {
